9e6c9be76fc0b8fe92e8d8f97e97c239d8b0b71a
jhonnierandrey <jhonnierandrey@outlook.com>
2020-09-26 18:21:01 -0300

    Visual improvements for Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,28 +11,6 @@ class Footer extends Component {
 							<img src="./pixabay-logo.svg" alt="Go to Pixabay"/>
 						</a>
 					</section>
-
-					{ /*
-					<section>
-						<h2>About</h2>
-						<p>This is a React App using the Pixabay API.</p>
-					</section>
-					
-					<section>
-						<h2>Follow</h2>
-						<ul className="icons">
-							<li><a href="/" className="icon brands style2 fa-twitter"><span className="label">Twitter</span></a></li>
-							<li><a href="/" className="icon brands style2 fa-facebook-f"><span className="label">Facebook</span></a></li>
-							<li><a href="/" className="icon brands style2 fa-instagram"><span className="label">Instagram</span></a></li>
-							<li><a href="/" className="icon brands style2 fa-dribbble"><span className="label">Dribbble</span></a></li>
-							<li><a href="/" className="icon brands style2 fa-github"><span className="label">GitHub</span></a></li>
-							<li><a href="/" className="icon brands style2 fa-500px"><span className="label">500px</span></a></li>
-							<li><a href="/" className="icon solid style2 fa-phone"><span className="label">Phone</span></a></li>
-							<li><a href="/" className="icon solid style2 fa-envelope"><span className="label">Email</span></a></li>
-						</ul>
-					</section>
-				
-					*/ }
 					<br />
 					<br />
 					<ul className="copyright">
