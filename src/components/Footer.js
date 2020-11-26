5ea154f37c7ed33551944662c272ebc6d63f4e88
jhonnierandrey <jhonnierandrey@outlook.com>
2020-11-25 21:13:56 -0300

    Improvements for Copyright

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,8 +13,7 @@ class Footer extends Component {
 				<ul className="footer-copyright">
 					<li>&copy; 2020 JAES Made It</li>
 					<li>Made with <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">React</a></li>
-					<li><a href="https://github.com/jhonnierandrey/dasimaginare" target="_blank" rel="noopener noreferrer">GitHub</a></li>
-					<li><a href="http://jhonnierandrey.info/" target="_blank" rel="noopener noreferrer">See more</a></li>
+					<li><a href="https://github.com/jhonnierandrey/dasimaginare" target="_blank" rel="noopener noreferrer">GitHub</a> | <a href="http://jhonnierandrey.info/" target="_blank" rel="noopener noreferrer">See more</a></li>
 				</ul>
 			</footer>
         );
