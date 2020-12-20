b8e10e0f27e4c48b3c82c74c93af257629b81438
jhonnierandrey <jhonnierandrey@outlook.com>
2020-12-19 21:13:02 -0300

    Change links order

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,8 +12,8 @@ class Footer extends Component {
 				</div>
 				<ul className="footer-copyright">
 					<li>&copy; 2020 <a href="https://www.jaesmadeit.com/" target="_blank" rel="noopener noreferrer">JAES Made It</a></li>
-					<li>Made with <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">React</a></li>
-					<li><a href="https://github.com/jhonnierandrey/dasimaginare" target="_blank" rel="noopener noreferrer">GitHub</a> | <a href="http://jhonnierandrey.info/" target="_blank" rel="noopener noreferrer">See more</a></li>
+					<li>Made with <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">React</a> | <a href="https://github.com/jhonnierandrey/dasimaginare" target="_blank" rel="noopener noreferrer">GitHub</a></li>
+					<li><a href="http://jhonnierandrey.info/" target="_blank" rel="noopener noreferrer">See more</a></li>
 				</ul>
 			</footer>
         );
