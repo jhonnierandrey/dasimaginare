0e8992eeb81b32b9a8b5abab39be567370132ec2
jhonnierandrey <jhonnierandrey@outlook.com>
2021-01-02 18:42:37 -0300

    new navbar added

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,18 +7,29 @@ class Header extends React.Component {
   render() {
       return(
           <header id="header">
+
+            <nav className="navbar">
+              <a className="navbar-brand" href="/">
+                  <img src={logo} alt="" width="30" height="30" className="d-inline-block align-top" /> Imaginäre
+              </a>
+            </nav>
+
             <div className="header-logo">
               <a href="/">
                 <img src={logo} alt="Imaginäre" />
               </a>
+
+              <h1>Welcome to IMAGINÄRE</h1>
+              <h1>Start searching for great Free images now</h1>
             </div>
 
-            <h1>Welcome to IMAGINÄRE</h1>
-            <p>Start searching for great Free images now</p>
-
-            <Search
-              searchParameters = {this.props.searchParameters}
-            />
+            <div className="header-searchbox">
+              <h1>Start searching for great Free images now</h1>
+              
+              <Search
+                searchParameters = {this.props.searchParameters}
+              />
+            </div>
 
           </header>
       )
