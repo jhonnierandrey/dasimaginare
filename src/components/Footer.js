3742041f1fa9a60c3ed15e1b5a34ca3920079dd6
jhonnierandrey <jhonnierandrey@outlook.com>
2021-01-04 23:41:52 -0300

    2021 Copyright updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,7 +11,7 @@ class Footer extends Component {
 					</a>
 				</div>
 				<ul className="footer-copyright">
-					<li>&copy; 2020 <a href="https://www.jaesmadeit.com/" target="_blank" rel="noopener noreferrer">JAES Made It</a></li>
+					<li>&copy; 2021 <a href="https://www.jaesmadeit.com/" target="_blank" rel="noopener noreferrer">JAES Made It</a></li>
 					<li>Made with <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">React</a> | <a href="https://github.com/jhonnierandrey/dasimaginare" target="_blank" rel="noopener noreferrer">GitHub</a></li>
 					<li><a href="http://jhonnierandrey.info/" target="_blank" rel="noopener noreferrer">See more</a></li>
 				</ul>
