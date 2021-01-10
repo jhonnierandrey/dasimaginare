a542f1103a84bf84c8f9c1b8b134bee49e8bc87a
jhonnierandrey <jhonnierandrey@outlook.com>
2021-01-10 15:31:06 -0300

    Improvements and cleaning for footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,17 +4,18 @@ class Footer extends Component {
     render() {
         return (
             <footer id="footer">
-				<div className="footer-credits">
-					<h2>We Use</h2>
-					<a href="https://pixabay.com/" target="_blank" rel="noopener noreferrer">
-						<img src="./pixabay-logo.svg" alt="Go to Pixabay"/>
+				<div className="footer-moveup">
+					<a href="#header" className="footer-moveup-btn">
+						<span>Go up <i className="fas fa-arrow-circle-up"></i></span>
 					</a>
 				</div>
+
 				<ul className="footer-copyright">
 					<li>&copy; 2021 <a href="https://www.jaesmadeit.com/" target="_blank" rel="noopener noreferrer">JAES Made It</a></li>
-					<li>Made with <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">React</a> | <a href="https://github.com/jhonnierandrey/dasimaginare" target="_blank" rel="noopener noreferrer">GitHub</a></li>
+					<li>Made with <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">React</a> + <a href="https://pixabay.com/" target="_blank" rel="noopener noreferrer"> Pixabay </a> + <a href="https://github.com/jhonnierandrey/dasimaginare" target="_blank" rel="noopener noreferrer">GitHub</a></li>
 					<li><a href="http://jhonnierandrey.info/" target="_blank" rel="noopener noreferrer">See more</a></li>
 				</ul>
+
 			</footer>
         );
     }
