6f8e0899d29e4a3077173ca6bfc309d6ffb6edb8
jhonnierandrey <jhonnierandrey@outlook.com>
2023-04-16 15:32:06 -0300

    0416 - migrating class components to functional components and code cleaning@

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,31 +1,29 @@
-import React, { Component } from 'react';
+import React from "react";
 
-class Search extends Component {
+export const Search = ({ searchParameters, query, setQuery }) => {
+  const getData = (e) => {
+    e.preventDefault();
+    searchParameters();
+  };
 
-    searchRef = React.createRef();
-    searchOrientation = React.createRef();
-
-    getData = (e) => {
-        e.preventDefault();
-        
-        const query = this.searchRef.current.value;
-        //const orientation = this.searchOrientation.current.value;
-
-        this.props.searchParameters(query);
-    }
-
-    render() {
-        return (
-            <form onSubmit={this.getData}>
-                <div className="form-row">
-                    <input className="form-control col-9" ref={this.searchRef} type="text" placeholder="Type your search here" maxLength="30"/>
-                    <button type="submit"   className="btn btn-secondary btn-lg col-3">
-                        <i className="fas fa-search"></i>
-                    </button>
-                </div>
-            </form>
-        );
-    }
-}
+  return (
+    <form onSubmit={getData}>
+      <div className="form-row">
+        <input
+          className="form-control col-9"
+          type="text"
+          name="search"
+          placeholder="Type your search here"
+          maxLength="30"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button type="submit" className="btn btn-secondary btn-lg col-3">
+          <i className="fas fa-search"></i>
+        </button>
+      </div>
+    </form>
+  );
+};
 
 export default Search;
